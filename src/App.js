164c7575d8d3fb7431fa98963ef1a55d87f4d8db
Giovanni Di Rosa <giodirosaartes@gmail.com>
2021-05-27 23:42:12 -0400

    Primer commit construccion de la app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import List from "./containers/List";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola a todos</h1>
+      <List />
+    </div>
+  );
+}
+
+export default App;
